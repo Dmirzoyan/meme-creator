77c2c5785c02fa3417bff91c923c0ec58006319e
Davit Mirzoyan <davit.mirzoyan@yahoo.com>
2019-02-03 21:17:37 +0100

    Styled textfields

--- MemeMe/ImageEditorViewController.swift
+++ MemeMe/ImageEditorViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 protocol ImageEditorInteracting {
 }
 
-class ImageEditorViewController: UIViewController {
+final class ImageEditorViewController: UIViewController {
 
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
@@ -20,13 +20,39 @@ class ImageEditorViewController: UIViewController {
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     
     private let isCameraAvailable = UIImagePickerController.isSourceTypeAvailable(.camera) ? true : false
+    
     private var interactor: ImageEditorInteracting {
         return ImageEditorInteractor(router: ImageEditorRouter(display: self))
     }
     
+    private var textAttributes: [NSAttributedString.Key: Any] {
+        return [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.strokeWidth: 2,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!
+        ]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         cameraButton.isEnabled = isCameraAvailable
+        topTextField.delegate = self
+        bottomTextField.delegate = self
+        
+        applyStyle()
+    }
+    
+    private func applyStyle() {
+        applyTextFieldStyle()
+    }
+    
+    private func applyTextFieldStyle() {
+        topTextField.defaultTextAttributes = textAttributes
+        bottomTextField.defaultTextAttributes = textAttributes
+        topTextField.text = "TOP"
+        bottomTextField.text = "BOTTOM"
     }
     
     @IBAction func selectAnImageFromLibrary(_ sender: Any) {
@@ -42,5 +68,17 @@ class ImageEditorViewController: UIViewController {
         imagePickerController.delegate = self
         
         present(imagePickerController, animated: true, completion: nil)
+    }
+}
+
+extension ImageEditorViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
 }
