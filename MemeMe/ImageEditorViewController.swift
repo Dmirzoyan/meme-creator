2029b464c98a70797eb01a88a5bd20627c9c762e
Davit Mirzoyan <davit.mirzoyan@yahoo.com>
2019-02-03 21:57:44 +0100

    Implemented keyboard slide

--- MemeMe/ImageEditorViewController.swift
+++ MemeMe/ImageEditorViewController.swift
@@ -41,7 +41,15 @@ final class ImageEditorViewController: UIViewController {
         topTextField.delegate = self
         bottomTextField.delegate = self
         
+        registerForKeyboardNotifications()
+        
         applyStyle()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        resignFromKeyboardNotifications()
     }
     
     private func applyStyle() {
@@ -55,6 +63,52 @@ final class ImageEditorViewController: UIViewController {
         bottomTextField.textAlignment = .center
         topTextField.text = "TOP"
         bottomTextField.text = "BOTTOM"
+    }
+    
+    private func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    func resignFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard
+            let userInfo = notification.userInfo,
+            let keyboardSize = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+            bottomTextField.isFirstResponder,
+            keyboardFrameIsChanged(userInfo)
+        else { return }
+        
+        view.frame.origin.y -= keyboardSize.cgRectValue.height
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        guard
+            let keyboardSize = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+            bottomTextField.isFirstResponder
+        else { return }
+        
+        view.frame.origin.y += keyboardSize.cgRectValue.height
+    }
+    
+    private func keyboardFrameIsChanged(_ keyboardInfo: [AnyHashable : Any]) -> Bool {
+        let keyboardSizeAtStart = keyboardInfo[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue
+        let keyboardSizeAtEnd = keyboardInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
+        
+        return keyboardSizeAtStart != keyboardSizeAtEnd
     }
     
     @IBAction func selectAnImageFromLibrary(_ sender: Any) {
