fb5b290df85af2c4fbcdd7e54e6023d87aa324d0
Davit Mirzoyan <davit.mirzoyan@yahoo.com>
2019-02-03 22:07:40 +0100

    Separated keyboard slide to a different extension file

--- MemeMe/ImageEditorViewController.swift
+++ MemeMe/ImageEditorViewController.swift
@@ -69,52 +69,6 @@ final class ImageEditorViewController: UIViewController {
         bottomTextField.text = "BOTTOM"
     }
     
-    private func registerForKeyboardNotifications() {
-        NotificationCenter.default.addObserver(
-            self, selector: #selector(keyboardWillShow),
-            name: UIResponder.keyboardWillShowNotification,
-            object: nil
-        )
-        
-        NotificationCenter.default.addObserver(
-            self, selector: #selector(keyboardWillHide),
-            name: UIResponder.keyboardWillHideNotification,
-            object: nil
-        )
-    }
-    
-    func resignFromKeyboardNotifications() {
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    @objc private func keyboardWillShow(_ notification: Notification) {
-        guard
-            let userInfo = notification.userInfo,
-            let keyboardSize = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
-            bottomTextField.isFirstResponder,
-            keyboardFrameIsChanged(userInfo)
-        else { return }
-        
-        view.frame.origin.y -= keyboardSize.cgRectValue.height
-    }
-    
-    @objc private func keyboardWillHide(_ notification: Notification) {
-        guard
-            let keyboardSize = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
-            bottomTextField.isFirstResponder
-        else { return }
-        
-        view.frame.origin.y += keyboardSize.cgRectValue.height
-    }
-    
-    private func keyboardFrameIsChanged(_ keyboardInfo: [AnyHashable : Any]) -> Bool {
-        let keyboardSizeAtStart = keyboardInfo[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue
-        let keyboardSizeAtEnd = keyboardInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
-        
-        return keyboardSizeAtStart != keyboardSizeAtEnd
-    }
-    
     @IBAction func selectAnImageFromLibrary(_ sender: Any) {
         openImagePicker()
     }
---
+++ MemeMe/KeyboardSlide.swift
@@ -0,0 +1,58 @@
+//
+//  KeyboardNotifications.swift
+//  MemeMe
+//
+//  Created by Davit Mirzoyan on 2/3/19.
+//  Copyright © 2019 Udacity. All rights reserved.
+//
+
+import UIKit
+
+extension ImageEditorViewController {
+    
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    func resignFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard
+            let userInfo = notification.userInfo,
+            let keyboardSize = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+            bottomTextField.isFirstResponder,
+            keyboardFrameIsChanged(userInfo)
+            else { return }
+        
+        view.frame.origin.y -= keyboardSize.cgRectValue.height
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        guard
+            let keyboardSize = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+            bottomTextField.isFirstResponder
+            else { return }
+        
+        view.frame.origin.y += keyboardSize.cgRectValue.height
+    }
+    
+    private func keyboardFrameIsChanged(_ keyboardInfo: [AnyHashable : Any]) -> Bool {
+        let keyboardSizeAtStart = keyboardInfo[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue
+        let keyboardSizeAtEnd = keyboardInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
+        
+        return keyboardSizeAtStart != keyboardSizeAtEnd
+    }
+}
