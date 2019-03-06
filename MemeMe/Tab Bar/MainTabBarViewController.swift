c9c4197ff6722eb2a8c72e7ed99dbf60dd13ce5f
Davit Mirzoyan <davit.mirzoyan@yahoo.com>
2019-03-06 02:24:53 +0100

    fixed view issues

--- MemeMe/Tab Bar/MainTabBarViewController.swift
+++ MemeMe/Tab Bar/MainTabBarViewController.swift
@@ -16,21 +16,19 @@ final class MainTabBarViewController: UITabBarController {
         applyStyle()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        addBarBorder()
+    }
+    
     private func applyStyle() {
         tabBar.tintColor = .white
         tabBar.barTintColor = UIColor.AppTheme.darkGrey
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-//        addBarBorder()
-    }
-    
     private func addBarBorder() {
         UITabBar.appearance().shadowImage = UIImage()
-        UITabBar.appearance().backgroundImage = UIImage()
-        
         let lineView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 0.5))
         lineView.backgroundColor = .gray
         tabBar.addSubview(lineView)
